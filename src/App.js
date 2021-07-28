8f15a732dd5620a0c047233b94a956aaf42f79ab
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-07-28 09:48:04 -0300

    useState controla o estado da funcao

--- src/App.js
+++ src/App.js
@@ -4,18 +4,33 @@ import { useState } from 'react';
 
 function App() {
   const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
   const reverseClass = reverse ? 'reverse' : '';
 
   const handleClick = () => {
     setReverse(!reverse);
+    setCounter(counter + 1);
+  };
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
   };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-        <button type="button" onClick={handleClick}>
-          Reverse: {reverseClass}
-        </button>
+
+        <h1>Counter: {counter} </h1>
+        <p>
+          <button type="button" onClick={handleClick}>
+            Reverse: {reverseClass}
+          </button>
+        </p>
+        <p>
+          <button type="button" onClick={handleIncrement}>
+            Reverse: {counter}
+          </button>
+        </p>
       </header>
     </div>
   );
