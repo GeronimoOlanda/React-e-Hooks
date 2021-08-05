a4b863844a5dabb7ae14821f4ca3c0f2f021f2b2
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-08-05 18:05:34 -0300

    useEffect e sua teoria

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
-  const reverseClass = reverse ? 'reverse' : '';
 
-  const handleClick = () => {
-    setReverse(!reverse);
-    setCounter(counter + 1);
-  };
-
-  const handleIncrement = () => {
-    setCounter((counter) => counter + 1);
-  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-
-        <h1>Counter: {counter} </h1>
-
-        <p>
-          <button type="button" onClick={handleClick}>
-            Reverse: {reverseClass}
-          </button>
-        </p>
-        <p>
-          <button type="button" onClick={handleIncrement}>
-            Reverse: {counter}
-          </button>
-        </p>
-      </header>
+      <h1>Contador: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
     </div>
   );
 }
+
 export default App;
