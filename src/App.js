13f6b168f5b2478a6ed3d20c402e0224e22ef90e
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-08-07 20:04:42 -0300

    usando parametro dentro do [], podemos definir a base na qual o useEffect sera atualizado

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ function App() {
 
   // executa toda vez que o component atualiza
   useEffect(() => {
-    console.log('Ola');
-  }, []);
+    console.log('ComponentDidUpdate');
+  });
+  // executa toda vez qye a dependencia mudar
+  useEffect(() => {
+    console.log(`Contador mudou para ${counter}`);
+  }, [counter]);
   return (
     <div className="App">
       <h1>Contador: {counter}</h1>
