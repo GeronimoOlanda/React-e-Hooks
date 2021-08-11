0e08cd625ebcb03a3fea8cef0606c139f82a208d
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-08-11 20:31:43 -0300

    Comentando o que ja foi entendido

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0);
-
+  const [counter2, setCounter2] = useState(0);
+  /*
   // executa toda vez que o component atualiza
   useEffect(() => {
     console.log('ComponentDidUpdate');
   });
+
   // executa toda vez qye a dependencia mudar
   useEffect(() => {
     console.log(`Contador mudou para ${counter}`);
   }, [counter]);
+ */
   return (
     <div className="App">
       <h1>Contador: {counter}</h1>
