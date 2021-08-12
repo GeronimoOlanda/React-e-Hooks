f8280506903cd1f0e3949ecea566b28426d0fd33
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-08-11 21:15:58 -0300

    Atualizando commentarios e os dados referente a o useEffect

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 function App() {
   const [counter, setCounter] = useState(0);
   const [counter2, setCounter2] = useState(0);
+
   /*
   // executa toda vez que o component atualiza
   useEffect(() => {
@@ -15,10 +16,18 @@ function App() {
     console.log(`Contador mudou para ${counter}`);
   }, [counter]);
  */
+  // toda variavel usada dentro do useEffect, deve ser passada como dependencia em []
+  useEffect(() => {
+    console.log('C1', counter, 'C2 ', counter2);
+  }, [counter, counter2]);
+
   return (
     <div className="App">
-      <h1>Contador: {counter}</h1>
+      <h1>
+        Contador: C1:{counter}, C2: {counter2}
+      </h1>
       <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter2(counter2 + 1)}>+ (2)</button>
     </div>
   );
 }
