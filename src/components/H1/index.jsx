0f444173c04010cd6b7ec7b6e195b17f10d39e9d
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-09-24 18:13:30 -0300

    Separando os components

--- src/components/H1/index.jsx
+++ src/components/H1/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { GlobalContext } from '../../contexts/AppContext';
 //eslint-disable-next-line
 const H1 = () => {
   const TheContext = useContext(GlobalContext); //usa o contexto fornecido pelo GlobalContext
--- src/contexts/AppContext/index.jsx
+++ src/contexts/AppContext/index.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import { globalState } from './data';
 //criamos um componente atraves do React.createContext();
 // esse componente tera duas coisas, um provider e o outro consumer(com useContext eliminamos o uso do consumer)
 // const GlobalContext = React.createContext(); cria um contexto global
-const GlobalContext = React.createContext();
+export const GlobalContext = createContext();
 
 //eslint-disable-next-line
 export const AppContext = ({ children }) => {
-  return <GlobalContext.Provider value={globalState}>{children}</GlobalContext.Provider>;
+  const [state, setState] = useState(globalState);
+
+  return <GlobalContext.Provider value={{ state, setState }}>{children}</GlobalContext.Provider>;
 };
