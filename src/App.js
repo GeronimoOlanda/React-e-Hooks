7bd5b30bfd58c5a0d069361d8a51d4cfdeebdb80
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-10-01 23:02:46 -0300

    refactory?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useReducer } from 'react';
 import './App.css';
-import { AppContext } from './contexts/AppContext';
-import { Div } from './components/Div';
+
+const globalState = {
+  title: 'O Caminho para o paraiso',
+  body: 'O corpo de cristo esta em contexto',
+  counter: 7,
+};
+
+const reducer = (state, action) => {
+  return { ...state };
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {' '}
+        {title} {body}{' '}
+      </h1>
+    </div>
   );
 }
 
