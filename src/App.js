d1f4b4b96825b27313d5943af3b3071ff191d6ae
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-10-18 09:13:35 -0300

    Invertendo strings

--- src/App.js
+++ src/App.js
@@ -4,22 +4,39 @@ import './App.css';
 const globalState = {
   title: 'O Caminho para o paraiso',
   body: 'O corpo de cristo esta em contexto',
-  counter: 7,
+  counter: 0,
 };
 
 const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda':
+      console.log('Chamou Muda');
+      return { ...state, title: 'mudou', counter: 1 };
+
+    case 'inverter':
+      console.log('inverteu');
+  }
+
   return { ...state };
 };
 
 function App() {
   const [state, dispatch] = useReducer(reducer, globalState);
+
   const { counter, title, body } = state;
   return (
     <div>
       <h1>
         {' '}
-        {title} {body}{' '}
+        {title} {counter}{' '}
       </h1>
+      <button
+        onClick={() => {
+          dispatch({ type: 'muda' });
+        }}
+      >
+        Click
+      </button>
     </div>
   );
 }
