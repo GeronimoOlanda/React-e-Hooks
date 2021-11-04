8eb07d7fc88dfed63a4414d27749f66434293cd4
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-11-04 12:08:06 -0300

    importando os dados

--- src/contexts/PostsProvider/index.jsx
+++ src/contexts/PostsProvider/index.jsx
@@ -1,8 +1,12 @@
 import P from 'prop-types';
 import { PostsContext } from './context';
+import { reducer } from './reduce';
+import { data } from './datas';
+import { useReducer } from 'react';
 
 // nosso component Provider
 export const PostsProvider = ({ children }) => {
+  const [postState, postDispatch] = useReducer(reducer, data);
   return <PostsContext.Provider>{children}</PostsContext.Provider>;
 };
 
