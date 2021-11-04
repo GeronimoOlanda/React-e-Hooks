ce4b4988e8b959813d27d1e5fd1a9518a70a5687
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-11-04 12:06:15 -0300

    criando nosso componente

--- src/contexts/PostsProvider/index.jsx
+++ src/contexts/PostsProvider/index.jsx
@@ -1,1 +1,11 @@
+import P from 'prop-types';
+import { PostsContext } from './context';
+
 // nosso component Provider
+export const PostsProvider = ({ children }) => {
+  return <PostsContext.Provider>{children}</PostsContext.Provider>;
+};
+
+PostsProvider.propTypes = {
+  children: P.node.isRequired,
+};
