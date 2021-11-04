316741fdb07faadfb6c49c3a3bbea6a26ce3fe7b
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-11-03 23:59:30 -0300

    refatorando projeto e organizando em pastas

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '.';
 
 it('renders learn react link', () => {
   render(<App />);
---
+++ src/templates/App/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <p>Oi</p>
+    </div>
+  );
+}
+
+export default App;
