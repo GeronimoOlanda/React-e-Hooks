549a56c6d658335d5d31bc42cbf176400c15f453
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-11-04 11:57:04 -0300

    criando nossa context e reducer

--- src/contexts/PostsProvider/context.js
+++ src/contexts/PostsProvider/context.js
@@ -1,1 +1,4 @@
+import { createContext } from 'react';
+
 // sera somente nosso createContext
+export const PostsContext = createContext();
--- src/contexts/PostsProvider/reduce.js
+++ src/contexts/PostsProvider/reduce.js
@@ -1,1 +1,12 @@
+import * as types from './types';
 // contem nosso reduce
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case types.POSTS_SUCCESS: {
+      console.log(action.type);
+      return { ...state, posts: action.payload };
+    }
+  }
+  console.log('action.type nao foi encontrada.');
+  return { ...state };
+};
