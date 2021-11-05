ccbeb529fd2f2496dc7510d749b19b513d838548
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-11-05 13:52:27 -0300

    chamando as action e context no component do post

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,12 +1,21 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
+import { loadPosts } from '../../contexts/PostsProvider/actions';
 import { PostsContext } from '../../contexts/PostsProvider/context';
 export const Posts = () => {
   const postsContext = useContext(PostsContext);
-  console.log(postsContext);
+  const { postsState, postsDispatch } = postsContext;
 
+  // carregando os posts
+  useEffect(() => {
+    loadPosts(postsDispatch);
+  }, [postsDispatch]);
   return (
     <div>
       <h1>Louvado o seja Jesus Cristo!</h1>
+
+      {postsState.posts.map((post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
     </div>
   );
 };
