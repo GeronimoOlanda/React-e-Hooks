664ecb8e4cf7b811c9001d5db9fbf55f8f1482aa
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-11-08 13:46:58 -0300

    up

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,9 +1,15 @@
 import { useContext, useEffect } from 'react';
+import { CounterContext } from '../../contexts/CounterProvider/context';
 import { loadPosts } from '../../contexts/PostsProvider/actions';
 import { PostsContext } from '../../contexts/PostsProvider/context';
+import { incrementCounter } from '../../contexts/CounterProvider/action';
+
 export const Posts = () => {
   const postsContext = useContext(PostsContext);
   const { postsState, postsDispatch } = postsContext;
+
+  const counterContext = useContext(CounterContext);
+  const { counterState, counterDispatch } = counterContext;
 
   // carregando os posts
   useEffect(() => {
@@ -11,6 +17,7 @@ export const Posts = () => {
   }, [postsDispatch]);
   return (
     <div>
+      <button onClick={() => incrementCounter(counterDispatch)}>Counter {counterState.counter} </button>
       <h1>Louvado o seja Jesus Cristo!</h1>
 
       {postsState.posts.map((post) => (
--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,14 +1,17 @@
 import './styles.css';
 import React from 'react';
 import { PostsProvider } from '../../contexts/PostsProvider';
+import { CounterProvider } from '../../contexts/CounterProvider';
 import { Posts } from '../../components/Posts';
 function App() {
   return (
-    <PostsProvider>
-      <div style={{ textAlign: 'center' }}>
-        <Posts />
-      </div>
-    </PostsProvider>
+    <CounterProvider>
+      <PostsProvider>
+        <div style={{ textAlign: 'center' }}>
+          <Posts />
+        </div>
+      </PostsProvider>
+    </CounterProvider>
   );
 }
 
