a7e48708e03749cad3699357366f49d95d82eb08
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-11-15 23:58:17 -0300

    formatação

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { PostsProvider } from '../../contexts/PostsProvider';
 import { CounterProvider } from '../../contexts/CounterProvider';
 import { Posts } from '../../components/Posts';
+
 function App() {
   return (
     <CounterProvider>
